fd0bc0b1235d60215511d47cf5d5210e0f0273ef
Trevor Pino <trevorpino@yahoo.com>
2020-10-05 14:07:15 -0600

    changes to PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const PORT = process.env.PORT || 3001;
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animal.json');
 
@@ -51,5 +51,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
+  console.log(`API server now on port ${PORT}!`);
 });
